4955c44aa52194b1e861d1794f26a860b1d5a68e
Daniel Ibáñez <danizgz95@gmail.com>
2024-11-27 23:23:19 +0100

    [27/11/2024] 23:23

---
+++ auto_pull.ps1
@@ -0,0 +1,8 @@
+# Cambia al directorio del repositorio
+Set-Location "C:\Users\daniz\Documents\GitHub\1SI"
+
+# Ejecuta git pull para actualizar el repositorio
+git pull
+
+# Muestra un mensaje indicando que se completó el pull
+Write-Output "El repositorio ha sido actualizado correctamente."
