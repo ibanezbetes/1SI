7d1c91d301b57420f83247723d126367567ee003
Daniel Ibáñez <danizgz95@gmail.com>
2025-02-13 13:38:04 +0100

    [13/02/2025] 13:38

---
+++ PROGRAMACION/SOLID_001/src/org/example/Printer.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Printer {
+    void printToHTML(){
+        System.out.println("Transformar en HTML");
+    }
+    void printToJSON(){
+        System.out.println("Transformar en JSON");
+    }
+    void printToXML(){
+        System.out.println("Transformar en XML");
+    }
+}
---
+++ PROGRAMACION/SOLID_001/src/org/example/herencia/Reporte.java
@@ -0,0 +1,13 @@
+package org.example.herencia;
+
+public class Reporte {
+    //ATRIBUTOS
+    protected String cadenaATransformar;
+    //CONSTRUCTOR
+    public Reporte(String cadena){
+        this.cadenaATransformar = cadena;
+    }
+    //MÉTODOS
+
+    //GETTER/SETTER
+}
